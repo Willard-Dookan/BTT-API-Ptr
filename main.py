1e1c255faa80410e1f16b8bab8ef990321c958fb
Willard-Dookan <max.dookan@gmail.com>
2025-03-13 18:39:07 -0400

    Adding main and task_model

---
+++ main.py
@@ -0,0 +1,18 @@
+from task_model import Task
+from fastapi import FastAPI
+
+app = FastAPI()
+
+tasks = [
+    Task(),
+    Task()
+]
+
+@app.get("/")
+def root():
+    return "Hello World"
+
+@app.get("/get-tasks")
+def get_all_tasks():
+    return tasks
+
