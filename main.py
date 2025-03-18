31dd42406a676772c7523492e5c7aff7048b77ee
Willard-Dookan <max.dookan@gmail.com>
2025-03-18 18:46:44 -0400

    reset files to Franklins

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# from task_router import task_router
 from task_model import Task
 from fastapi import FastAPI
 
@@ -5,30 +6,49 @@ app = FastAPI()
 
 tasks = [
     Task(),
-    Task(),
     Task()
 ]
+
 
 @app.get("/")
 def root():
     return "Hello World"
 
-@app.get("/get-tasks")
+@app.get("/tasks/all")
 def get_all_tasks():
     return tasks
 
-@app.get("/")
-def get_task():
-    return "Hello World"
+@app.post("/task")
+def create_task(task: Task):
+    tasks.append(task)
+    return "Task Added"
 
-@app.put("/")
-def update_task():
-    return "Hello World"
+@app.put("/{task_id}")
+def update_task(task_id: int, updated: Task):
+    for task in tasks:
+        if task.id == task_id:
+            task.description = updated.description
+            task.isComplete = updated.isComplete
+            return "Updated task"
+    
+    return "Task not found"
 
-@app.post("/")
-def create_task():
-    return "Hello World"
+@app.delete("/task/delete/{task_id}")
+def delete_task(task_id: int):
+    for index, task in enumerate(tasks):
+        if task.id == task_id:
+            tasks.pop(index)
+            return "Deleted task"
+    return "Task not found"
 
-@app.delete("/")
-def delete_task():
-    return "Hello World"
+@app.get("/get-task/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task.id == task_id:
+            return task
+    return "Not found"
+
+
+
+
+# app.include_router(task_router)
